51ed130bacb8a5973305d9495b83001170fc4e30
Jared Giangrasso <jagiangrasso@gmail.com>
2019-04-02 16:37:55 -0400

    adjust form for responsiveness

--- client/src/components/AddForm/Input.js
+++ client/src/components/AddForm/Input.js
@@ -4,7 +4,7 @@ import './Input.css';
 let Input = (props) => {
 	return (
 		<div className='form-group'>
-			<label htmlFor={props.name}></label>
+			<label htmlFor={props.name}>{props.name}</label>
 			<input
 				className='form-input'
 				id={props.name}
